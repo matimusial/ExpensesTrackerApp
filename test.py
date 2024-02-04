7f5db370ab6f821ecef83f240e0b806ddf3135e2
Mateusz <matimusial5@gmail.com>
2024-02-04 01:14:09 +0100

    third

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-print("Hello World")
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+import sys
 
+
+class App(QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        self.loadBaseCalcUI()
+        self.show()
+
+    def loadBaseCalcUI(self):
+        uic.loadUi("untitled.ui", self)
+
+app = QApplication(sys.argv)
+window = App()
+app.exec_()
