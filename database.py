7899c530e9c4dc6a56c5c77da1e32c579f3942c4
Mateusz <matimusial5@gmail.com>
2024-02-07 02:16:44 +0100

    hash and AES

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import mysql.connector
-
+import bcrypt
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 class Database:
 
@@ -15,7 +18,8 @@ class Database:
                 host=self.url.split('@')[1].split('/')[0],
                 user=self.url.split('://')[1].split(':')[0],
                 password=self.url.split(':')[2].split('@')[0],
-                database=self.url.split('/')[-1]
+                database=self.url.split('/')[-1],
+                ssl_disabled = False
             )
         except mysql.connector.Error:
             self.error = "Błąd połączenia z internetem, zrestartuj aplikację."
@@ -25,6 +29,14 @@ class Database:
             self.connection.close()
 
     def register(self, first_name, last_name, login, password):
+
+        password = self.hash(password)
+
+        try:
+            first_name = self.encrypt(first_name)
+            last_name = self.encrypt(last_name)
+        except Exception as e:
+            print(e)
 
         cursor = self.connection.cursor()
         cursor.execute(
@@ -44,14 +56,31 @@ class Database:
                 return False
         return True
 
-    def login(self, username, password):
+    def login(self, username, password, hashed):
         pass
 
     def hash(self, password):
-        pass
 
-    def encrypt(self):
-        pass
+        byte = password.encode('utf-8')
+        hashed = bcrypt.hashpw(byte, bcrypt.gensalt())
+
+        return hashed.decode('utf-8')
+
+    def encrypt(self, data):
+
+        try:
+            with open('key.bin', 'rb') as file:
+                key = file.read()
+        except Exception:
+            print("Wrong AES-192 key path")
+
+        cipher = AES.new(key, AES.MODE_CBC)
+
+        data = data.encode('utf-8')
+
+        ciphertext = cipher.encrypt(pad(data, AES.block_size))
+
+        return cipher.iv + ciphertext
 
     def decrypt(self):
         pass
